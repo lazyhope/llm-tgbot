754f40b5e9a054833832a8fa3a020be0a779d798
Zihao Li <zl81@st-andrews.ac.uk>
2023-12-20 14:20:24 +0800

    Add chat history for gpt query, use dotenv to configure env vars like api tokens and allowed users, use logging to replace debug message

--- bot.py
+++ bot.py
@@ -1,22 +1,41 @@
 import asyncio
 import os
 
+from dotenv import load_dotenv
 from faster_whisper import WhisperModel
 from langchain.chat_models import ChatAnthropic
-from telebot import asyncio_filters, asyncio_helper
+from telebot import asyncio_filters, asyncio_helper, logger, logging
 from telebot.async_telebot import AsyncTeleBot
 from telebot.callback_data import CallbackData
 from telebot.types import CallbackQuery, Message
 
+from message_db import MessageDB
+
+load_dotenv()
+
+# Bot setup
 asyncio_helper.proxy = os.getenv("HTTPS_PROXY")
-allowed_users = []
 bot = AsyncTeleBot(token=os.getenv("TG_BOT", ""))
+logger.setLevel(logging.INFO)
 
-claude = ChatAnthropic(model="claude-2.1", max_tokens=1024, temperature=0)
-
-chat_model = claude
-whisper = WhisperModel("large-v2", device="cpu", compute_type="int8")
+# Handler filters
+allowed_users = [
+    int(user_id) for user_id in os.getenv("ALLOWED_USERS", "").split(",") if user_id
+]
 voice_callback = CallbackData("task", prefix="voice_callback")
+
+# Database setup
+db = MessageDB()
+
+# Model setup
+chat_model = ChatAnthropic(model="claude-2.1", max_tokens=4096, temperature=0)
+whisper = WhisperModel("small", device="cpu", compute_type="int8")
+
+
+@bot.message_handler(chat_id=allowed_users, commands=["clear"])
+async def clear_history(message: Message):
+    db.clear_history(message)
+    await bot.reply_to(message, text="History cleared.")
 
 
 @bot.message_handler(chat_id=allowed_users, commands=["dalle"])
@@ -44,9 +63,11 @@ async def generate_image(message: Message):
             chat_id=message.chat.id, photo=image.url, caption=image.revised_prompt
         )
     except openai.OpenAIError as e:
+        error_message = f"[!] Bot exception:\n{e}"
+        logger.error(error_message)
         await bot.reply_to(
             message,
-            text=f"[!] Bot exception:\n{e}",
+            text=error_message,
         )
 
 
@@ -90,6 +111,7 @@ async def voice_callback_handler(call: CallbackQuery):
 
     task = callback_data.get("task")
     if task == "ask_gpt":
+        call.message.from_user.is_bot = False
         await chat_query(call.message)
     elif task == "dream":
         await generate_image(call.message)
@@ -107,12 +129,11 @@ async def chat_query(message: Message):
     api_task = asyncio.create_task(bot.reply_to(message, text="Generating..."))
     message_sent = await api_task
 
-    bot_response = ""
-    user_query = message.text
-    print(f"[User] {user_query}")
-    async for chunk in chat_model.astream(user_query):
-        print(chunk.content, end="", flush=True)
+    db.add_message(message)
+    messages = db.get_all_history(message)  # Retrieve history including current query
 
+    bot_response = ""
+    async for chunk in chat_model.astream(messages):
         bot_response += chunk.content
         if api_task.done() and bot_response.strip():
             api_task = asyncio.create_task(
@@ -125,8 +146,6 @@ async def chat_query(message: Message):
 
         await asyncio.sleep(0.1)
 
-    print(f"\n[Bot] {bot_response}")
-    print("[+] Chat message completed.")
     if not bot_response.strip():
         await bot.edit_message_text(
             text="No response received.",
@@ -134,6 +153,10 @@ async def chat_query(message: Message):
             message_id=message_sent.message_id,
         )
         return
+
+    message_sent = await api_task
+    message_sent.text = bot_response  # Prevent incomplete message stored
+    db.add_message(message_sent)
 
     try:
         await bot.edit_message_text(
@@ -143,20 +166,15 @@ async def chat_query(message: Message):
             parse_mode="HTML",
         )
     except asyncio_helper.ApiTelegramException as e:
-        print(f"[!] Error Log: {e.description}")
         if "message is not modified" not in e.description:
+            error_message = f"[!] Bot exception:\n{e.description}"
+            logger.error(error_message)
             await bot.reply_to(
                 message_sent,
-                text=f"[!] Bot exception:\n{e.description}",
+                text=error_message,
             )
-
-
-@bot.message_handler(chat_id=allowed_users, commands=["clear"])
-async def clear_history(message: Message):
-    chat_id = message.chat.id
 
 
 bot.add_custom_filter(asyncio_filters.ChatFilter())
 
-print("[+] Bot severing...")
-asyncio.run(bot.polling())
+asyncio.run(bot.infinity_polling())
